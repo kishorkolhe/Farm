950e8f4401cb0c7389fa163e254b0867737af6bd
Kishor_PC <Kishor_PC@Kishor>
2018-09-11 22:42:20 +0530

    chnages in Application file

--- FarmERP/src/main/java/com/impace/FarmErpApplication.java
+++ FarmERP/src/main/java/com/impace/FarmErpApplication.java
@@ -2,8 +2,11 @@ package com.impace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 
 @SpringBootApplication
+@ComponentScans(value= {@ComponentScan(basePackages="com.impace")})
 public class FarmErpApplication {
 
 	public static void main(String[] args) {
